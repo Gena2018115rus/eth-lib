423bfc94c873d484094ec6a11eef71564b6ee04d
Gennady Lebedev <mr.gennadiy.lebedev@gmail.com>
2022-05-09 02:28:04 +0300

    made the header linux-undepended
    dll compilation support

--- eth-lib.cpp
+++ eth-lib.cpp
@@ -1,7 +1,45 @@
 #include "eth-lib.hpp"
+#include <unistd.h>
 #include <arpa/inet.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <iostream>
+#include <regex>
 
-listener_t::listener_t(unsigned short port) :
+struct client_addr_t
+{
+    struct sockaddr data;
+    socklen_t len;
+};
+
+class listener_impl_t : public listener_t {
+  public:
+    listener_impl_t(unsigned short port);
+    
+    void run(void (*onNewClient)(client_ref_t), void (*onNewMessage)(const char *, const char *, size_t)) override;
+    ~listener_impl_t() override;
+    void send_to_all(const char *buf, size_t count) override;
+    
+  private:
+    // unsigned short m_port;
+    int m_sockfd;
+    static inline const int INT_ONE = 1;
+//    std::mutex sockfds_mtx;
+    std::vector<pollfd> m_sockfds;
+    std::map<int, client_addr_t> m_client_addrs; // client_addrs[fd].len is unused?     sorted_map избыточен?
+    std::unordered_map<int, std::string> m_client_in_bufs;
+    bool m_must_break;
+    
+    friend client_ref_t;
+    
+    void disconnect(int client_fd);
+};
+
+listener_impl_t::listener_impl_t(unsigned short port) :
 //   m_port(port),
   m_sockfd{} {
     struct addrinfo *servinfo;
@@ -58,22 +96,21 @@ listener_t::listener_t(unsigned short port) :
     m_sockfds.push_back({m_sockfd, POLLIN, 0});
 }
 
-listener_t::~listener_t() {
+listener_impl_t::~listener_impl_t() {
     close(m_sockfd);
 }
 
-client_ref_t::client_ref_t(listener_t &listener, int sockfd) :
+client_ref_t::client_ref_t(listener_t *listener, int sockfd) :
   m_listener(listener),
   m_sockfd(sockfd) {
-    
 }
 
-bool client_ref_t::write(std::string buf) {
-    if (::write(m_sockfd, buf.data(), buf.size()) == -1) {
+bool client_ref_t::write(const char *buf, size_t count) {
+    if (::write(m_sockfd, buf, count) == -1) {
         if (errno == EPIPE)
         {
 //                        to_remove_csfds.push_back(client_sockfd); // сообщать о выходе человека
-            std::cerr << "write(" << m_sockfd << ") => EPIPE -- странно??" << std::endl;
+            std::cerr << "write(" << m_sockfd << ") => EPIPE -- странно??" << std::endl; // сделать чтобы в файл логировалось и не трогало глобальные стримы
         }
         else
         {
@@ -85,11 +122,11 @@ bool client_ref_t::write(std::string buf) {
     return true;
 }
 
-const std::string &client_ref_t::input_buf() {
-    return m_listener.m_client_in_bufs.at(m_sockfd);
+const char *client_ref_t::input_buf() {
+    return dynamic_cast<listener_impl_t *>(m_listener)->m_client_in_bufs.at(m_sockfd).data();
 }
 
-void listener_t::disconnect(int client_fd) {
+void listener_impl_t::disconnect(int client_fd) {
     // т.к. инвалидируются итераторы, после этого вызова нельзя продолжать итерации, т.е. нужен break
     m_sockfds.erase(std::find_if(m_sockfds.begin(), m_sockfds.end(), [client_fd](auto pollfd) { return pollfd.fd == client_fd; })); // инвалидируются итераторы!
 //                    send_to_all(addr_in_2str((struct sockaddr_in *)&client_addrs[fd].data) + " disconnected!");
@@ -99,22 +136,38 @@ void listener_t::disconnect(int client_fd) {
 }
 
 void client_ref_t::disconnect() {
-    m_listener.disconnect(m_sockfd);
+    dynamic_cast<listener_impl_t *>(m_listener)->disconnect(m_sockfd);
     close(m_sockfd);
 }
 
-std::string client_ref_t::addr() {
-    auto addr_in = (struct sockaddr_in *)&m_listener.m_client_addrs.at(m_sockfd).data;
-    return std::string(inet_ntoa(addr_in->sin_addr)) + ':' + std::to_string(addr_in->sin_port);
+const char *client_ref_t::addr() {
+    static std::string save;
+    
+    auto addr_in = (struct sockaddr_in *)&dynamic_cast<listener_impl_t *>(m_listener)->m_client_addrs.at(m_sockfd).data;
+    save = inet_ntoa(addr_in->sin_addr) + ':' + std::to_string(addr_in->sin_port);
+    return save.data();
 }
 
-void listener_t::send_to_all(std::string message) {
+void listener_impl_t::send_to_all(const char *buf, size_t count) {
     for (auto client_it = m_sockfds.begin() + 1; client_it != m_sockfds.end(); ++client_it) {
-        client_ref_t(*this, client_it->fd).write(message);
+        client_ref_t(this, client_it->fd).write(buf, count);
     }
 }
 
-client_t::client_t(const std::string &addr, const std::string &port) :
+class client_impl_t : public client_t {
+  public:
+    client_impl_t(const std::string &addr, const std::string &port);
+
+    void listen(void (*onNewData)(const char *, size_t)) override;
+    bool write(const char *message, size_t sz) override;
+    ~client_impl_t() override;
+
+  private:
+    std::string m_addr, m_port;
+    int m_sockfd;
+};
+
+client_impl_t::client_impl_t(const std::string &addr, const std::string &port) :
   m_addr(addr),
   m_port(port),
   m_sockfd(socket(AF_INET, SOCK_STREAM, 0)) {
@@ -126,6 +179,7 @@ client_t::client_t(const std::string &addr, const std::string &port) :
 
     struct addrinfo *servinfo;
     const struct addrinfo hints = {
+        .ai_flags = 0,
         .ai_family = AF_INET,
         .ai_socktype = SOCK_STREAM,
     };
@@ -151,12 +205,148 @@ client_t::client_t(const std::string &addr, const std::string &port) :
     freeaddrinfo(servinfo);
 }
 
-bool client_t::write(std::string buf) {
-    client_t tmp(m_addr, m_port);
-    buf = "POST /message HTTP/1.1\r\nContent-Length: " + std::to_string(buf.size()) + "\r\n\r\n" + buf;
+bool client_impl_t::write(const char *message, size_t sz) {
+    client_impl_t tmp(m_addr, m_port);
+    std::string buf = "POST /message HTTP/1.1\r\nContent-Length: " + std::to_string(sz) + "\r\n\r\n" + std::string{message, message + sz};
     return ::write(tmp.m_sockfd, buf.data(), buf.size()) == buf.size();
 }
 
-client_t::~client_t() {
+client_impl_t::~client_impl_t() {
     // close(m_sockfd);
+}
+
+void listener_impl_t::run(void (*onNewClient)(client_ref_t), void (*onNewMessage)(const char *, const char *, size_t)) {
+    for (;;)
+    {
+        for (auto& sockfd : m_sockfds) sockfd.revents = 0; // need?
+//            sockfds_mtx.lock();
+        int poll_res = poll(m_sockfds.data(), m_sockfds.size(), -1 /*20*/);
+//            sockfds_mtx.unlock();
+
+        if (poll_res < 0)
+        {
+            std::cerr << "poll() error!" << std::endl;
+            errno = 0;
+        }
+
+        if (m_sockfds[0].revents & (POLLHUP | POLLERR | POLLNVAL))
+        {
+            std::cerr << "server_socket error!" << std::endl;
+            exit(0);
+        }
+        else if (m_sockfds[0].revents & POLLIN)
+        {
+            struct sockaddr client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            int client_sockfd = accept(m_sockfd, &client_addr, &client_addr_len);
+            if (client_sockfd < 0)
+            {
+                std::cerr << "accept() error." << std::endl;
+//                    continue; // ? exit()
+            }
+            else
+            {
+                m_client_addrs[client_sockfd] = {client_addr, client_addr_len};
+//                    send_to_all(addr_in_2str((struct sockaddr_in *)&client_addr) + " connected!");
+
+                client_ref_t c(this, client_sockfd);
+                const char buf[] = "HTTP/1.1 200 OK\r\n\r\n";
+                c.write(buf, sizeof(buf) - 1);
+                onNewClient(std::move(c));
+
+                m_sockfds.push_back({client_sockfd, POLLIN, 0}); // надо будет наверное операции с sockfds защитить мьютексами
+//                    close(client_sockfd);
+            }
+        }
+
+        for (auto client_it = m_sockfds.begin() + 1; client_it != m_sockfds.end(); ++client_it)
+        {
+            int client_fd = client_it->fd;
+            // auto client_addr = m_client_addrs[client_fd].data;
+            if (client_it->revents & (POLLHUP | POLLERR | POLLNVAL))
+            {
+                disconnect(client_fd);
+                break;
+            }
+            else if (client_it->revents & POLLIN)
+            {
+                m_must_break = false;
+                for (char in_buf[16];;) {
+                    ssize_t bytes_read = recv(client_fd, in_buf, sizeof(in_buf), MSG_DONTWAIT); // т.к. я работаю с raw data, я никогда не должен полагаться на \0
+                    if (bytes_read > 0) {
+                        // то добавить в буфер и считать ещё из сокета
+                        m_client_in_bufs[client_fd] += {in_buf, in_buf + bytes_read};
+                    } else if (bytes_read == 0) { // this is eof (т.к. у меня SOCK_STREAM (TCP))
+                        disconnect(client_fd);
+                        // надо close(client_fd)?     видимо да, т.к. если нет eof, я могу вызвать close(), но во время вызова может стать eof, соответственно можно close() на eof'нутый сокет делать
+                        goto break_2;
+                    } else /* bytes_read < 0 */ {
+                        // error    or     ret_val = -1 and errno is EAGAIN or EWOULDBLOCK means that there are no data ready, try again later
+                        if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                            std::cerr << "read(" << client_fd << ") error. errno = " << strerror(errno) << std::endl;
+                        } else {
+                            // there are no data ready, try again later
+                            // МБ ПОМОЖЕТ В ОТВЕТЕ ПОСЛАТЬ content-length: 0 и пустую строку? чтобы сюда не заходить выполнению
+                            // std::cerr << "уже считано " << std::regex_replace(client_in_bufs[client_fd], std::regex{"[^]*?\r\n\r\n"}, "", std::regex_constants::format_first_only).size() << std::endl;
+                        }
+                        errno = 0;
+                        
+                        std::string input_buf = m_client_in_bufs[client_fd];
+                        std::smatch splitted;
+                        if (regex_match(input_buf, splitted, std::regex{"([^]*?\r\n)\r\n([^]*)"})) { // мб сильно сэкономит память если я буду не .str(2) делать, а только 1 захвал и .suffix()
+                            auto headers = splitted.str(1);
+                            std::smatch m;
+                            if (regex_search(headers, m, std::regex{"Content-Length: (\\d+)\r\n"})
+                                && splitted.str(2).size() >= std::stoull(m[1])) {
+                                client_ref_t client(this, client_fd);
+                                std::string addr = client.addr();
+                                client.disconnect();
+
+                                auto index = input_buf.find("\r\n");
+                                bool flag = false;
+                                if (index != (size_t)-1) {
+                                    std::string s = {input_buf.begin(), input_buf.begin() + index};
+                                    flag = regex_search(s, std::regex{"^POST /message"});
+                                }
+
+                                if (flag) {
+                                    auto msg = std::regex_replace(input_buf, std::regex{"[^]*?\r\n\r\n"}, "", std::regex_constants::format_first_only);
+                                    onNewMessage(addr.data(), msg.data(), msg.size());
+                                } else {
+                                    std::clog << input_buf << std::endl;
+                                }
+                            }
+                        }
+                        
+                        if (m_must_break) {
+                            goto break_2;
+                        }
+                        
+                        break;
+                    }
+                }
+            }
+        }
+        break_2:;
+    }
+}
+
+void client_impl_t::listen(void (*onNewData)(const char *, size_t)) {
+    for (char in_buf[1024];;) {
+        ssize_t n = read(m_sockfd, in_buf, 1024);
+        if (n > 0) {
+            onNewData(in_buf, n);
+        } else {
+            std::clog << "disconnected from server" << std::endl;
+            exit(0);
+        }
+    }
+}
+
+listener_t *make_listener(unsigned short port) {
+    return new listener_impl_t(port);
+}
+
+client_t *make_client(const char *addr, const char *port) {
+    return new client_impl_t(addr, port);
 }
--- eth-lib.hpp
+++ eth-lib.hpp
@@ -1,203 +1,48 @@
 #ifndef ETH_LIB
 #define ETH_LIB
 
-#include <poll.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <string.h>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <string>
-#include <iostream>
-#include <regex>
 
 class listener_t;
 
-struct client_addr_t
-{
-    struct sockaddr data;
-    socklen_t len;
-};
-
 class client_ref_t {
   public:
-    client_ref_t(listener_t &listener, int sockfd);
-    bool write(std::string buf);
-    const std::string &input_buf();
+    client_ref_t(listener_t *listener, int sockfd);
+    virtual bool write(const char *buf, size_t count);
+    const char *input_buf(); // аккуратно при использовании не из потока сервера!
     void disconnect();
-    std::string addr();
+    const char *addr(); // аккуратно при использовании не из потока сервера!
 
   private:
-    listener_t &m_listener;
+    listener_t *m_listener;
     int m_sockfd;
 };
 
 class listener_t {
   public:
-    listener_t(unsigned short port);
+    listener_t() = default;
     
-    // onNewClient_t  = void(client_ref_t client);
-    // onNewMessage_t = void(std::string addr, std::string msg);
-    template <typename onNewClient_t, typename onNewMessage_t>
-    void run(onNewClient_t onNewClient, onNewMessage_t onNewMessage) {
-        for (;;)
-        {
-            for (auto& sockfd : m_sockfds) sockfd.revents = 0; // need?
-    //            sockfds_mtx.lock();
-            int poll_res = poll(m_sockfds.data(), m_sockfds.size(), -1 /*20*/);
-    //            sockfds_mtx.unlock();
+    // onNewClient_t  = void(*)(client_ref_t client);
+    // onNewMessage_t = void(*)(const char *addr, const char *msg, size_t msg_sz);
+    virtual void run(void (*onNewClient)(client_ref_t), void (*onNewMessage)(const char *, const char *, size_t)) = 0;
 
-            if (poll_res < 0)
-            {
-                std::cerr << "poll() error!" << std::endl;
-                errno = 0;
-            }
-
-            if (m_sockfds[0].revents & (POLLHUP | POLLERR | POLLNVAL))
-            {
-                std::cerr << "server_socket error!" << std::endl;
-                exit(0);
-            }
-            else if (m_sockfds[0].revents & POLLIN)
-            {
-                struct sockaddr client_addr;
-                socklen_t client_addr_len = sizeof(client_addr);
-                int client_sockfd = accept(m_sockfd, &client_addr, &client_addr_len);
-                if (client_sockfd < 0)
-                {
-                    std::cerr << "accept() error." << std::endl;
-    //                    continue; // ? exit()
-                }
-                else
-                {
-                    m_client_addrs[client_sockfd] = {client_addr, client_addr_len};
-    //                    send_to_all(addr_in_2str((struct sockaddr_in *)&client_addr) + " connected!");
-
-                    client_ref_t c(*this, client_sockfd);
-                    c.write("HTTP/1.1 200 OK\r\n\r\n");
-                    onNewClient(std::move(c));
-
-                    m_sockfds.push_back({client_sockfd, POLLIN, 0}); // надо будет наверное операции с sockfds защитить мьютексами
-    //                    close(client_sockfd);
-                }
-            }
-
-            for (auto client_it = m_sockfds.begin() + 1; client_it != m_sockfds.end(); ++client_it)
-            {
-                int client_fd = client_it->fd;
-                // auto client_addr = m_client_addrs[client_fd].data;
-                if (client_it->revents & (POLLHUP | POLLERR | POLLNVAL))
-                {
-                    disconnect(client_fd);
-                    break;
-                }
-                else if (client_it->revents & POLLIN)
-                {
-                    m_must_break = false;
-                    for (char in_buf[16];;) {
-                        ssize_t bytes_readed = recv(client_fd, in_buf, sizeof(in_buf), MSG_DONTWAIT); // т.к. я работаю с raw data, я никогда не должен полагаться на \0
-                        if (bytes_readed > 0) {
-                            // то добавить в буфер и считать ещё из сокета
-                            m_client_in_bufs[client_fd] += {in_buf, in_buf + bytes_readed};
-                        } else if (bytes_readed == 0) { // this is eof (т.к. у меня SOCK_STREAM (TCP))
-                            disconnect(client_fd);
-                            // надо close(client_fd)?     видимо да, т.к. если нет eof, я могу вызвать close(), но во время вызова может стать eof, соответственно можно close() на eof'нутый сокет делать
-                            goto break_2;
-                        } else /* bytes_readed < 0 */ {
-                            // error    or     ret_val = -1 and errno is EAGAIN or EWOULDBLOCK means that there are no data ready, try again later
-                            if (errno != EWOULDBLOCK && errno != EAGAIN) {
-                                std::cerr << "read(" << client_fd << ") error. errno = " << strerror(errno) << std::endl;
-                            } else {
-                                // there are no data ready, try again later
-                                // МБ ПОМОЖЕТ В ОТВЕТЕ ПОСЛАТЬ content-length: 0 и пустую строку? чтобы сюда не заходить выполнению
-                                // std::cerr << "уже считано " << std::regex_replace(client_in_bufs[client_fd], std::regex{"[^]*?\r\n\r\n"}, "", std::regex_constants::format_first_only).size() << std::endl;
-                            }
-                            errno = 0;
-//----------------------------------------------------------------------------
-                            std::string input_buf = m_client_in_bufs[client_fd];
-                            std::smatch splitted;
-                            if (regex_match(input_buf, splitted, std::regex{"([^]*?\r\n)\r\n([^]*)"})) { // мб сильно сэкономит память если я буду не .str(2) делать, а только 1 захвал и .suffix()
-                                auto headers = splitted.str(1);
-                                std::smatch m;
-                                if (regex_search(headers, m, std::regex{"Content-Length: (\\d+)\r\n"})
-                                    && splitted.str(2).size() >= std::stoull(m[1])) {
-                                    client_ref_t client(*this, client_fd);
-                                    auto addr = client.addr();
-                                    client.disconnect();
-
-                                     auto index = input_buf.find("\r\n");
-                                     bool flag = false;
-                                     if (index != (size_t)-1) {
-                                         std::string s = {input_buf.begin(), input_buf.begin() + index};
-                                         flag = regex_search(s, std::regex{"^POST /message"});
-                                     }
-
-                                     if (flag) {
-//                                    if (input_buf.starts_with("POST /message")) { // clang on androin fails here and version above doesn't work on desktop?
-                                        onNewMessage(addr, std::regex_replace(input_buf, std::regex{"[^]*?\r\n\r\n"}, "", std::regex_constants::format_first_only));
-                                    } else {
-                                        std::clog << input_buf << std::endl;
-                                    }
-                                }
-                            }
-//----------------------------------------------------------------------------
-                            if (m_must_break) {
-                                goto break_2;
-                            }
-                            
-                            break;
-                        }
-                    }
-                }
-            }
-            break_2:;
-        }
-    }
-
-    ~listener_t();
-    void send_to_all(std::string message);
-    
-  private:
-    // unsigned short m_port;
-    int m_sockfd;
-    static inline const int INT_ONE = 1;
-//    std::mutex sockfds_mtx;
-    std::vector<pollfd> m_sockfds;
-    std::map<int, client_addr_t> m_client_addrs; // client_addrs[fd].len is unused?     sorted_map избыточен?
-    std::unordered_map<int, std::string> m_client_in_bufs;
-    bool m_must_break;
-    
-    friend client_ref_t;
-    
-    void disconnect(int client_fd);
+    virtual ~listener_t() = default;
+    virtual void send_to_all(const char *buf, size_t count) = 0;
 };
+
+listener_t *make_listener(unsigned short port);
 
 class client_t {
   public:
-    client_t(const std::string &addr, const std::string &port);
+    client_t() = default;
 
-    // onNewData_t = void(std::string chunk);
-    template <typename onNewData_t>
-    void listen(onNewData_t onNewData) {
-        for (char in_buf[1024];;) {
-            ssize_t n = read(m_sockfd, in_buf, 1024);
-            if (n > 0) {
-                onNewData({in_buf, in_buf + n});
-            } else {
-                std::clog << "disconnected from server" << std::endl;
-                exit(0);
-            }
-        }
-    }
+    // onNewData_t = void(*)(const char *chunk, size_t sz);
+    virtual void listen(void (*onNewData)(const char *, size_t)) = 0;
 
-    bool write(std::string buf);
-    ~client_t();
-
-  private:
-    std::string m_addr, m_port;
-    int m_sockfd;
+    virtual bool write(const char *message, size_t sz) = 0;
+    virtual ~client_t() = default;
 };
+
+client_t *make_client(const char *addr, const char *port);
 
 #endif // #ifndef ETH_LIB
